3f653aacb693880314829cf4914356e3659f2a14
James Conte <James@mld.com>
2025-10-02 11:48:48 -0600

    moving api file structure around

--- api/index.js
+++ api/index.js
@@ -1,11 +1,8 @@
 // api/index.js
-import app from "../src/app.js";
+import app from '../src/app.js';
 
 export default function handler(req, res) {
-  // Make Express see the same paths you used before (/api/products, etc.)
-  if (req.url.startsWith('/api/')) req.url = req.url.slice(4);
 
-  // (Optional) fast-path OPTIONS; your cors() middleware should also handle this
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', 'https://www.mld.com');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
